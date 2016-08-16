64375f91fe50b76509c752bf300d06c297850852
Andrey <fi93@mail.ru>
2016-08-16 22:20:07 +0200

    add ;

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Article extends Component {
 
     state = {
         isOpen: false
-    }
+    };
 
     render() {
-        const { article } = this.props
-        const body = this.state.isOpen ? <section>{article.text}</section> : null
+        const { article } = this.props;
+        const body = this.state.isOpen ? <section>{article.text}</section> : null;
         return (
             <div>
                 <h3 onClick = {this.handleClick}>{article.title}</h3>
@@ -20,6 +20,6 @@ export default class Article extends Component {
     handleClick = (ev) => {
         this.setState({
             isOpen: !this.state.isOpen
-        })
-    }
+        });
+    };
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Article from './Article'
+import React from 'react';
+import Article from './Article';
 
 export default function ArticleList(props) {
-    const articleItems = props.articles.map(articleObject => <li key = {articleObject.id}><Article article = {articleObject}/></li>)
+    const articleItems = props.articles.map(articleObject => <li key = {articleObject.id}><Article article = {articleObject}/></li>);
     return (
         <ul>
             {articleItems}
